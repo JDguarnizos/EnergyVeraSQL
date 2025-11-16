3edae67fe0486e089af547ec669617d8e18c804a
ElSanster <134859584+ElSanster@users.noreply.github.com>
2025-11-16 16:24:02 -0500

    chore: creación de proyecto en maven
    Vamos a ver que sale

---
+++ EnergyVeraSQL/EnergyVeraSQL/src/main/java/com/energyvera/energyverasql/EnergyVeraSQL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.energyvera.energyverasql;
+
+/**
+ *
+ * @author PC-PERSONAL
+ */
+public class EnergyVeraSQL {
+
+    public static void main(String[] args) {
+        System.out.println("Archivo main por defecto ejecutado (Cambia esto xd).");
+    }
+}
