7d12983b7efa4246f28a794a6097976ff83f9804
ValentinaVera <vveral@ucompensar.edu.co>
2025-11-16 17:51:12 -0500

    Update producto.java

--- EnergyVeraSQL/src/main/java/com/energyvera/model/producto.java
+++ EnergyVeraSQL/src/main/java/com/energyvera/model/producto.java
@@ -1,13 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.energyvera.model;
 
-/**
- *
- * @author valen
- */
 public class producto {
     
+    private int idProducto;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int idProveedor;
+
+    public producto() {
+    }
+
+    public producto(int idProducto, String nombre, String descripcion, double precio, int idProveedor) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.idProveedor = idProveedor;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
 }
