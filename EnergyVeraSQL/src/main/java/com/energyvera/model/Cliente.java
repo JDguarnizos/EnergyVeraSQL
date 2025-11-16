3fd3339af399f007e6abbaf2effbb886bf62f7fe
ValentinaVera <vveral@ucompensar.edu.co>
2025-11-16 17:38:46 -0500

    Update Cliente.java

--- EnergyVeraSQL/src/main/java/com/energyvera/model/Cliente.java
+++ EnergyVeraSQL/src/main/java/com/energyvera/model/Cliente.java
@@ -1,9 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.energyvera.model;
 
 public class Cliente {
-    
+
+    private int idCliente;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int idCliente, String nombre, String apellido, String direccion, String telefono, String correo) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
+
 }
