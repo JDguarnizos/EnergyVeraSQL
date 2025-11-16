257c5b55ecd19651c72e610e5d38e490eb0649e8
ValentinaVera <vveral@ucompensar.edu.co>
2025-11-16 18:38:51 -0500

    Update proveedor.java

--- EnergyVeraSQL/src/main/java/com/energyvera/model/proveedor.java
+++ EnergyVeraSQL/src/main/java/com/energyvera/model/proveedor.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.energyvera.model;
 
-/**
- *
- * @author valen
- */
 public class proveedor {
     
+    private int idProveedor;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+    public proveedor() {
+    }
+
+    public proveedor(int idProveedor, String nombre, String telefono, String email, String direccion) {
+        this.idProveedor = idProveedor;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public int getIdProveedor() {
+        return idProveedor;
+    }
+
+    public void setIdProveedor(int idProveedor) {
+        this.idProveedor = idProveedor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    @Override
+    public String toString(){
+    return nombre + " - " + idProveedor;
+    }
 }
