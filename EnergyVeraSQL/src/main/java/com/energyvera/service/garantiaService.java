956dc27017340449469c0db6d71fc6c69ffb19c8
ValentinaVera <vveral@ucompensar.edu.co>
2025-11-16 20:31:30 -0500

    Update garantiaService.java

--- EnergyVeraSQL/src/main/java/com/energyvera/service/garantiaService.java
+++ EnergyVeraSQL/src/main/java/com/energyvera/service/garantiaService.java
@@ -13,27 +13,22 @@ public class garantiaService {
         this.garantiaDAO = new GarantiaDAOImpl();
     }
 
-    // Registrar nueva garantía
     public void registrarGarantia(garantia gar) {
         garantiaDAO.insertar(gar);
     }
 
-    // Actualizar garantía existente
     public void actualizarGarantia(garantia gar) {
         garantiaDAO.actualizar(gar);
     }
 
-    // Eliminar garantía por ID
     public void eliminarGarantia(int id) {
         garantiaDAO.eliminar(id);
     }
 
-    // Buscar una garantía por ID
     public garantia buscarGarantiaPorId(int id) {
         return garantiaDAO.obtenerPorId(id);
     }
 
-    // Listar todas las garantías
     public List<garantia> listarGarantias() {
         return garantiaDAO.obtenerTodos();
     }
