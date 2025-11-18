585c0f9e3f3f36fa848758007d9d67e3c21f9259
ElSanster <134859584+ElSanster@users.noreply.github.com>
2025-11-17 21:32:44 -0500

    feat (forms): Formularios de Garantía Finalizados
    Con este commit me explico mejor como hice los otros 2 formaularios del anterior: Copio y pego el formulario de FacturaVenta y realizo las adaptaciones al tipo de tabla al que aplicará los cambios.
    
    Es mucho más fácil que tener que hacer desde 0 un formulario, adaptarlo y corregir errores.

--- EnergyVeraSQL/src/main/java/com/energyvera/view/MainMenu.java
+++ EnergyVeraSQL/src/main/java/com/energyvera/view/MainMenu.java
@@ -228,19 +228,19 @@ public class MainMenu extends javax.swing.JFrame {
     }//GEN-LAST:event_Inventario_JButtonActionPerformed
 
     private void Garantias_JButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Garantias_JButtonActionPerformed
-        FrmGarantias ventanaGa = new FrmGarantias();
+        FrmGarantias ventanaGa = new FrmGarantias(garService); //Por hacer
         ventanaGa.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_Garantias_JButtonActionPerformed
 
     private void Vendedores_JButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Vendedores_JButtonActionPerformed
-        FrmVendedores ventanaVe = new FrmVendedores();
+        FrmVendedores ventanaVe = new FrmVendedores(); //Por hacer
         ventanaVe.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_Vendedores_JButtonActionPerformed
 
     private void Proveedores_JButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Proveedores_JButtonActionPerformed
-        FrmProveedores ventanaProv = new FrmProveedores();
+        FrmProveedores ventanaProv = new FrmProveedores(); /Por hacer
         ventanaProv.setVisible(rootPaneCheckingEnabled);
         this.dispose();
     }//GEN-LAST:event_Proveedores_JButtonActionPerformed
